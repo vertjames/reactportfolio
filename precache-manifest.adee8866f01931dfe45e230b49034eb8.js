560ccfcc97ce31f82a5579750b67f93c9817bb63
vertjames <ianandrewkirby@gmail.com>
2020-06-23 15:50:16 -0700

    Updates

---
+++ precache-manifest.adee8866f01931dfe45e230b49034eb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d4cd7a015523c969988dee429d012f",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "cbe32704ff80e55a92d0",
+    "url": "/reactportfolio/static/css/main.51760bd3.chunk.css"
+  },
+  {
+    "revision": "171569dd826be68c9acd",
+    "url": "/reactportfolio/static/js/2.ccb4eea9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.ccb4eea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe32704ff80e55a92d0",
+    "url": "/reactportfolio/static/js/main.0d5377fc.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "58673ad0f77a6e0443980220346e3437",
+    "url": "/reactportfolio/static/media/First-Blood.58673ad0.pdf"
+  },
+  {
+    "revision": "7c0ac939583392c7cef7c2fcd0576a5d",
+    "url": "/reactportfolio/static/media/rambo.7c0ac939.png"
+  },
+  {
+    "revision": "bcf8784370cd722e77a4d763d8b74593",
+    "url": "/reactportfolio/static/media/stallone-logo.bcf87843.png"
+  }
+]);
